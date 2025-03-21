79444c1d9a5805f911ba3b97b493992d5dfb3cd8
Gabriel Carvalho <gc201319@gmail.com>
2025-03-21 14:30:27 -0300

    coisas

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>App Help Desk</title>
+    <link rel="stylesheet" href="style.css">
+
+  </head>
+  <body>
+    <header>
+      <nav>
+        <img src="logo.png" alt="logo">
+        <h1>HelpDesk</h1>
+      </nav>
+    </header>
+  </body>
+</html>
